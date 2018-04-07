d81db926a7a2c3f37149e6af3f7cd115c0c0419e
Taimuraz <parabotic@gmail.com>
2018-04-07 22:11:43 +0300

    added text edit.

--- src/main.py
+++ src/main.py
@@ -2,11 +2,13 @@ from tkinter import *
 from enum import Enum
 from math import sqrt
 
+
 class DrawingMode(Enum):
     NONE = 0
     BS = 1
     F = 2
     T = 3
+
 
 class Node:
     x = 0
@@ -19,35 +21,36 @@ class Node:
         self.node_type = node_type
 
     def __repr__(self):
-        return 'x={}, y={}, type={}'.format(self.x , self.y, self.node_type)
+        return 'x={}, y={}, type={}'.format(self.x, self.y, self.node_type)
 
 
 class MainView:
 
-    def __init__(self, master, minimal_distance = 0,width=1024, height=700):
+    def __init__(self, master, minimal_distance=0, width=1024, height=700):
         self.bs_color = 'blue'
         self.f_color = 'green'
         self.t_color = 'red'
         self.node_radius = 30
         self.minimal_distance = minimal_distance
-
         self.mode = DrawingMode.NONE
-
         self.nodes = []
 
         master.resizable(width=False, height=False)
         master.geometry('{}x{}'.format(width, height))
-
         menu_frame = Frame(master)
         menu_frame.pack(side=RIGHT, anchor='n')
 
-        self.btn_bs = Button(menu_frame, text="БС")
+        self.btn_bs = Button(menu_frame, text="Добавить БС")
         self.btn_bs.pack(fill=X)
 
-        self.btn_f = Button(menu_frame, text="Ф узел")
+        self.btn_f = Button(menu_frame, text="Добавить Ф узел")
         self.btn_f.pack(fill=X)
 
-        self.btn_t = Button(menu_frame, text="Т узел")
+        self.entry_widg = Entry(menu_frame)
+        self.entry_widg.insert(0, '9')
+        self.entry_widg.pack(fill=X)
+
+        self.btn_t = Button(menu_frame, text="Сгенерировать Т узлы")
         self.btn_t.pack(fill=X)
 
         self.btn_exit = Button(menu_frame, text="Выход", fg="red", command=menu_frame.quit)
@@ -71,20 +74,21 @@ class MainView:
         return result
 
     def drawCircle(self, x, y, color):
-        self.canvas.create_oval([x - self.node_radius, y - self.node_radius], [x + self.node_radius, y + self.node_radius], fill=color)
+        self.canvas.create_oval([x - self.node_radius, y - self.node_radius],
+                                [x + self.node_radius, y + self.node_radius], fill=color)
 
     def onCanvasClick(self, event):
         x = event.x
         y = event.y
         if self.isValidDistance(x, y):
             if self.mode == DrawingMode.BS:
-                self.drawCircle( x, y, self.bs_color)
+                self.drawCircle(x, y, self.bs_color)
                 self.nodes.append(Node(x, y, 'bs'))
             elif self.mode == DrawingMode.F:
-                self.drawCircle( x, y, self.f_color)
+                self.drawCircle(x, y, self.f_color)
                 self.nodes.append(Node(x, y, 'f'))
             elif self.mode == DrawingMode.T:
-                self.drawCircle( x, y, self.t_color)
+                self.drawCircle(x, y, self.t_color)
                 self.nodes.append(Node(x, y, 't'))
         print(self.nodes[0])
 
@@ -107,7 +111,7 @@ class MainView:
 root = Tk()
 canv = Canvas(root, width=30, height=30)
 canv.pack()
-app = MainView(root, minimal_distance = 20)
+app = MainView(root, minimal_distance=20)
 
 root.mainloop()
 root.destroy()  # optional; see description below
